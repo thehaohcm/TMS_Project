242aeee72db638b563f5c2edbca8d88c764cb9b2
ansidev <ansidev@gmail.com>
2017-05-24 00:01:04 +0700

    Config Application

--- src/main/java/org/fsoft/tms/TmsApplication.java
+++ src/main/java/org/fsoft/tms/TmsApplication.java
@@ -1,12 +1,16 @@
 package org.fsoft.tms;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
+@EnableJpaRepositories
 public class TmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TmsApplication.class, args);
+    }
 }
