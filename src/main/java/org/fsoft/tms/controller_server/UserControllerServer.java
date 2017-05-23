b0f44b2cb2949efb521aebe390a5034a6814dc4a
Camtm <Camtm95@gmail.com>
2017-05-23 11:06:23 +0700

    Merge branch 'mockproject' of https://gitlab.com/thehaohcm/TMS_Project
    # Conflicts:
    #	src/main/java/org/fsoft/tms/entity/Course.java

--- src/main/java/org/fsoft/tms/controller_server/UserControllerServer.java
+++ src/main/java/org/fsoft/tms/controller_server/UserControllerServer.java
@@ -1,16 +1,23 @@
 package org.fsoft.tms.controller_server;
 
+import org.fsoft.tms.service.ServiceUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by DELL on 5/23/2017.
  */
 @Controller
-@RequestMapping(value = "/tms/user")
+@RequestMapping(value = "/tms/server/user")
 public class UserControllerServer {
-    @RequestMapping(value = "/get")
-    public String getListUser() {
+    @Autowired
+    ServiceUser user;
+
+    @RequestMapping(value = "/getall")
+    public String getListUser(Model model) {
+        model.addAttribute("listUser", user.getListUser());
         return "them";
     }
 }
--- src/main/java/org/fsoft/tms/service/ServiceUser.java
+++ src/main/java/org/fsoft/tms/service/ServiceUser.java
@@ -1,7 +1,21 @@
 package org.fsoft.tms.service;
 
+import org.fsoft.tms.entity.User;
+import org.fsoft.tms.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by DELL on 5/23/2017.
  */
+@Service
 public class ServiceUser {
+    @Autowired
+    private UserRepository service;
+
+    public List<User> getListUser() {
+        return service.findAll();
+    }
 }
