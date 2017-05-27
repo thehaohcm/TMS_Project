87fcb2c339783e9842fa52decc54f790425a5b49
Camtm <Camtm95@gmail.com>
2017-05-27 15:30:02 +0700

    Merge branch 'mockproject' of https://gitlab.com/thehaohcm/TMS_Project
    # Conflicts:
    #	hsql/data/tms.lck

--- src/main/java/org/fsoft/tms/service/UserPropertyService.java
+++ src/main/java/org/fsoft/tms/service/UserPropertyService.java
@@ -1,7 +1,22 @@
 package org.fsoft.tms.service;
 
+import org.fsoft.tms.entity.User;
+import org.fsoft.tms.entity.UserProperty;
+import org.fsoft.tms.repository.UserPropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by DELL on 5/27/2017.
  */
+@Service
 public class UserPropertyService {
+    @Autowired
+    UserPropertyRepository userProperty;
+
+    public List<UserProperty> getListUserProperty(User user) {
+        return userProperty.getAllByPk_User(user);
+    }
 }
