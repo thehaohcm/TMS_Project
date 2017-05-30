2c0f0d2bf5e8ae6f235f6968f896e0efadf9bc8a
thehaohcm <thehaohcm@yahoo.com.vn>
2017-05-30 17:41:04 +0700

    Nguyễn Thế Hào 17h40 30-05-2017

--- src/main/java/org/fsoft/tms/controller/TrainerController.java
+++ src/main/java/org/fsoft/tms/controller/TrainerController.java
@@ -1,15 +1,11 @@
 package org.fsoft.tms.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.fsoft.tms.CurrentUser;
-import org.fsoft.tms.entity.Course;
-import org.fsoft.tms.entity.Topic;
-import org.fsoft.tms.entity.User;
-import org.fsoft.tms.entity.UserProperty;
+import org.fsoft.tms.entity.*;
 import org.fsoft.tms.repository.RoleRepository;
-import org.fsoft.tms.service.RoleService;
-import org.fsoft.tms.service.TopicService;
-import org.fsoft.tms.service.UserPropertyService;
-import org.fsoft.tms.service.UserService;
+import org.fsoft.tms.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.jws.WebParam;
 import javax.jws.soap.SOAPBinding;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -32,10 +29,15 @@ public class TrainerController
     UserService userService;
 
     @Autowired
+    PropertyService propertyService;
+
+    @Autowired
     UserPropertyService userPropertyService;
 
     @Autowired
     TopicService topicService;
+
+    private final Logger logger = LogManager.getLogger();
 
     @RequestMapping(value = "/")
     public String getPageIndex(Model model) {
@@ -53,16 +55,85 @@ public class TrainerController
     @RequestMapping(value = "/update/{id}")
     public String getPageUpdate(@PathVariable String id, Model model) {
         User user = userService.findOneUser(Integer.parseInt(id));
-        model.addAttribute("user", user);
-        model.addAttribute("listUserProperty", userPropertyService.getListUserProperty(user));
+        TrainerInfo trainerInfo = new TrainerInfo();
+        trainerInfo.setUser(user);
+        trainerInfo.setName(userPropertyService.getUserProperty(user,
+                propertyService.findOneProperty(1)).getValue());
+        trainerInfo.setEmail(userPropertyService.getUserProperty(user,
+                propertyService.findOneProperty(10)).getValue());
+        trainerInfo.setPhone(userPropertyService.getUserProperty(user,
+                propertyService.findOneProperty(9)).getValue());
+        trainerInfo.setAddress(userPropertyService.getUserProperty(user,
+                propertyService.findOneProperty(8)).getValue());
+        model.addAttribute("trainer", trainerInfo);
+//        model.addAttribute("listUserProperty", userPropertyService.getListUserProperty(user));
         return "trainer/update";
     }
 
     @RequestMapping(value = "/updateProfile")
-    public String Update(@ModelAttribute User user) {
+    public String Update(@ModelAttribute TrainerInfo trainerInfo) {
+        logger.debug(trainerInfo.getUser().getUsername());
+
+        Set<UserProperty> userProperties = new HashSet<>(0);
+
+        logger.debug("-1:"+trainerInfo.getName());
+
+        userProperties = userPropertyService.setTrainerProperty(trainerInfo.getUser(), trainerInfo.getName(),
+                trainerInfo.getEmail(), trainerInfo.getPhone(), trainerInfo.getAddress());
+        for(UserProperty userProperty : userProperties)
+            logger.debug("0:"+userProperty.getValue());
+
+        User user = userService.findOneUser(trainerInfo.getUser().getId());
+        user.setUserProperties(userProperties);
+        logger.debug("1:"+user.getUsername());
+        logger.debug("2:"+user.getPassword());
+        Set<UserProperty> userProperties1 = user.getUserProperties();
+        for(UserProperty userProperty : userProperties1)
+            logger.debug("3:"+userProperty.getValue());
+//        logger.debug("4:"+user.getManager().toString());
+
         userService.saveUser(user);
         return "redirect:/tms/trainer/";
     }
+
+//    @RequestMapping(value = "/update/{id}")
+//    public String getPageUpdate(@PathVariable String id, Model model) {
+//        User user = userService.findOneUser(Integer.parseInt(id));
+////        TrainerInfo trainerInfo = new TrainerInfo();
+////        trainerInfo.setUser(user);
+////        trainerInfo.setName(userPropertyService.getUserProperty(user,
+////                propertyService.findOneProperty(1)).getValue());
+////        trainerInfo.setEmail(userPropertyService.getUserProperty(user,
+////                propertyService.findOneProperty(10)).getValue());
+////        trainerInfo.setPhone(userPropertyService.getUserProperty(user,
+////                propertyService.findOneProperty(9)).getValue());
+////        trainerInfo.setAddress(userPropertyService.getUserProperty(user,
+////                propertyService.findOneProperty(8)).getValue());
+//        model.addAttribute("nameProperty", userPropertyService.getUserProperty(user,
+//                propertyService.findOneProperty(1)));
+//        model.addAttribute("emailProperty", userPropertyService.getUserProperty(user,
+//                propertyService.findOneProperty(10)));
+//        model.addAttribute("phoneProperty", userPropertyService.getUserProperty(user,
+//                propertyService.findOneProperty(9)));
+//        model.addAttribute("addressProperty", userPropertyService.getUserProperty(user,
+//                propertyService.findOneProperty(10)));
+//        return "trainer/update";
+//    }
+//
+//    @RequestMapping(value = "/updateProfile")
+//    public String Update(@ModelAttribute UserProperty name, UserProperty email, UserProperty phone, UserProperty address) {
+//        logger.debug("1:"+name.getValue());
+//        logger.debug("2:"+email.getValue());
+//        logger.debug("3:"+phone.getValue());
+//        logger.debug("4:"+address.getValue());
+//        List<UserProperty> userProperties = new ArrayList<>();
+//        userProperties.add(name);
+//        userProperties.add(email);
+//        userProperties.add(phone);
+//        userProperties.add(address);
+//        userPropertyService.saveTrainerProperty(userProperties);
+//        return "redirect:/tms/trainer/";
+//    }
 
 
     @RequestMapping(value = "/topic")
@@ -81,4 +152,6 @@ public class TrainerController
         model.addAttribute("listCourse", topicService.findAllCourseOfUser(currentUser.getUser()));
         return "course";
     }
+
+
 }
